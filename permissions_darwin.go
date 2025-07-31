0cb0309361797487fa847bcc8493d73cf3978a07
Itxaka <itxaka@kairos.io>
2025-07-31 17:39:16 +0200

    Implement elevated permissions check with admin prompt for macOS

--- permissions_darwin.go
+++ permissions_darwin.go
@@ -3,9 +3,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func CheckElevatedPermissions() (bool, error) {
-	return os.Geteuid() == 0, nil
+	if os.Geteuid() != 0 {
+		// Re-exec with osascript and admin prompt
+		exePath, _ := filepath.Abs(os.Args[0])
+		cmd := exec.Command("osascript", "-e", fmt.Sprintf(`
+		do shell script "%s" with administrator privileges with prompt "Kairos-must-burn needs permission to write to your USB device."`, exePath))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			os.Exit(1)
+		}
+		return true, nil
+	}
+	os.Exit(0)
+	return false, nil
 }
